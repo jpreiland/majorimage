4b00596e38005f4e4bcf0f51e12cd0daa8f0a665
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2023-03-11 16:00:19 -0800

    updated special gerunds, added an experimental descriptor format

--- client/src/components/locations/templates/city/BasicCity.vue
+++ client/src/components/locations/templates/city/BasicCity.vue
@@ -3,8 +3,7 @@
     <p>
       <Descriptor :type="'City'" />
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. <Descriptor :type="'Book'" :color="'red'"/>
-      hendrerit, ligula vitae feugiat ullamcorper, velit <Descriptor :type="'City'" /> placerat nisi, 
-      sit <Descriptor :type="'Temple'" :color="'green'"/> vestibulum <Descriptor :type="'City'" /> enim et magna.Lorem <Descriptor :type="'City'" /> dolor sit amet, consectetur adipiscing elit. Integer <Descriptor :type="'City'" />, ligula vitae feugiat ullamcorper, 
+      hendrerit, ligula vitae feugiat ullamcorper, velit <Descriptor  :type="'EventVerbing'" :color="'gold'" /> placerat nisi, sit <Descriptor :type="'Temple'" :color="'green'"/> vestibulum <Descriptor :type="'City'" /> enim et magna.Lorem <Descriptor :type="'City'" /> dolor sit amet, consectetur adipiscing elit. Integer <Descriptor :type="'City'" />, ligula vitae feugiat ullamcorper, 
       velit nulla placerat nisi, sit amet vestibulum sapien <Descriptor :type="'CreatureVerbingObject(s)'" :color="'blue'"/> et magna.<Descriptor :type="'City'" /> ipsum dolor sit <Descriptor :type="'City'" />, consectetur adipiscing elit. <Descriptor :type="'City'" /> hendrerit, 
       ligula vitae feugiat ullamcorper.
       Lorem ipsum dolor sit amet, consectetur adipiscing <Descriptor :type="'City'" />. Integer hendrerit, ligula vitae feugiat ullamcorper, velit nulla placerat nisi, 
