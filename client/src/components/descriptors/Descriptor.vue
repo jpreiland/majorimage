fdd415fc06addec874352944981c410ec19a854b
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2023-03-11 20:33:36 -0800

    added support for 'a(n)' special format part

--- client/src/components/descriptors/Descriptor.vue
+++ client/src/components/descriptors/Descriptor.vue
@@ -6,6 +6,7 @@
 import pluralize from 'pluralize'
 import tensify from '../../lib/tensify/inflector'
 import gerund from '../../lib/gerunds/gerunds'
+import AvsAnSimple from '../../lib/a-vs-an/avs-an-simple'
 
 export default {
   name: "Descriptor",
@@ -22,7 +23,8 @@ export default {
   data () {
     return {
       descriptorText: "default name",
-      formatPicker: []
+      formatPicker: [],
+      a_an_flag: false
     }
   },
   async mounted() {
@@ -40,24 +42,33 @@ export default {
     },
     buildName(parts) {
       let name = ""
+      let word
       let numWords = 1
 
       for (let part of parts) {
         switch (part[0]) {
           case 'pick':
-            name += this.wordPicker(part[1])
+            word = this.wordPicker(part[1])
+            name += this.do_a_an(word)
             break;
 
           case 'pick-pluralize':
+            if (this.a_an_flag) {
+              name += "some "
+              this.a_an_flag = false
+            }
             name += pluralize(this.wordPicker(part[1]))
             break;
 
           case 'pick-pluralize-optional':
+            word = this.wordPicker(part[1])
+
             if (Math.random() >= 0.5) {
-              name += pluralize(this.wordPicker(part[1]))
-            } else {
-              name += this.wordPicker(part[1])
+              name += "some " + pluralize(word)
+              break;
             }
+
+            name += this.do_a_an(word)
             break;
 
           case 'pick-pastTense':
@@ -85,19 +96,27 @@ export default {
             break;
 
           case 'pick-group':
-            name += this.groupWordPicker(part[1])
+            word = this.groupWordPicker(part[1])
+            name += this.do_a_an(word)
             break;
 
           case 'pick-group-pluralize':
+            if (this.a_an_flag) {
+              name += "some "
+              this.a_an_flag = false
+            }
             name += pluralize(this.groupWordPicker(part[1]))
             break;
 
           case 'pick-group-pluralize-optional':
+            word = this.groupWordPicker(part[1])
+
             if (Math.random() >= 0.5) {
-              name += pluralize(this.groupWordPicker(part[1]))
-            } else {
-              name += this.groupWordPicker(part[1])
+              name += "some " + pluralize(word)
+              break;
             }
+
+            name += this.do_a_an(word)
             break;
 
           case 'pick-group-pastTense':
@@ -126,7 +145,8 @@ export default {
 
           case 'pick-multi':
             numWords = part[2] ? (Math.floor(Math.random() * part[2]) + 1) : 1
-            name += this.wordPicker(part[1])
+            word = this.wordPicker(part[1])
+            name += this.do_a_an(word)
             numWords--
 
             while (numWords > 0) {
@@ -137,7 +157,8 @@ export default {
 
           case 'pick-group-multi':
             numWords = part[2] ? (Math.floor(Math.random() * part[2]) + 1) : 1
-            name += this.groupWordPicker(part[1])
+            word = this.groupWordPicker(part[1])
+            name += this.do_a_an(word)
             numWords--
 
             while (numWords > 0) {
@@ -150,12 +171,22 @@ export default {
             name += part[1]
             break;
 
+          case 'a(n)':
+            name += " "
+            this.a_an_flag = true
+            break;
+
           default:
             break;
         }
       }
 
       return name
+    },
+    do_a_an(word) {
+      if (!this.a_an_flag) return word
+      this.a_an_flag = false
+      return AvsAnSimple.query(word) + " " + word
     },
     wordPicker(category) {
       return this.wordData.words[category][Math.floor(Math.random() * this.wordData.words[category].length)]
--- client/src/components/locations/templates/city/BasicCity.vue
+++ client/src/components/locations/templates/city/BasicCity.vue
@@ -3,10 +3,10 @@
     <p>
       <Descriptor :type="'City'" />
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. <Descriptor :type="'Book'" :color="'red'"/>
-      hendrerit, ligula vitae feugiat ullamcorper, velit <Descriptor  :type="'EventVerbing'" :color="'gold'" /> placerat nisi, sit <Descriptor :type="'Temple'" :color="'green'"/> vestibulum <Descriptor :type="'City'" /> enim et magna.Lorem <Descriptor :type="'City'" /> dolor sit amet, consectetur adipiscing elit. Integer <Descriptor :type="'City'" />, ligula vitae feugiat ullamcorper, 
+      hendrerit, ligula vitae feugiat ullamcorper, velit <Descriptor  :type="'EventVerbing'" :color="'gold'" /> placerat nisi, sit <Descriptor :type="'Temple'" :color="'green'"/> vestibulum <Descriptor :type="'TestA(n)'" :color="'orange'" /> enim et magna.Lorem <Descriptor :type="'City'" /> dolor sit amet, consectetur adipiscing elit. Integer <Descriptor :type="'City'" />, ligula vitae feugiat ullamcorper, 
       velit nulla placerat nisi, sit amet vestibulum sapien <Descriptor :type="'CreatureVerbingObject(s)'" :color="'blue'"/> et magna.<Descriptor :type="'City'" /> ipsum dolor sit <Descriptor :type="'City'" />, consectetur adipiscing elit. <Descriptor :type="'City'" /> hendrerit, 
       ligula vitae feugiat ullamcorper.
-      Lorem ipsum dolor sit amet, consectetur adipiscing <Descriptor :type="'City'" />. Integer hendrerit, ligula vitae feugiat ullamcorper, velit nulla placerat nisi, 
+      Lorem ipsum dolor sit amet, consectetur adipiscing <Descriptor :type="'A(n)AdjectivePersonalityTradeNPC'" :color="'silver'"/>. Integer hendrerit, ligula vitae feugiat ullamcorper, velit nulla placerat nisi, 
       sit amet vestibulum sapien enim et magna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit, ligula vitae feugiat ullamcorper, 
       velit nulla placerat nisi, sit amet vestibulum sapien enim et magna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit, 
       ligula vitae feugiat ullamcorper.
