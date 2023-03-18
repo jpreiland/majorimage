55e64bea9f9c52baba38e0cbaf8eb7f47dc046e5
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2023-03-18 13:03:03 -0700

    WIP: support for nested word groups

--- routes/api/data.js
+++ routes/api/data.js
@@ -27,6 +27,10 @@ router.get('/materials', async (req, res) => {
 
 router.get('/data', async (req, res) => {
   try {
+    const wordCounts = buildWordCounts()
+    const compiledWordGroupsNew = compileWordGroupsNew(wordCounts)
+
+
     const compiledWordGroups = compileWordGroups()
     const compiledFormats = compileFormats()
     const compiledTemplates = compileTemplates()
@@ -54,6 +58,86 @@ router.get('/locations', async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 })
+
+function buildWordCounts() {
+  wordCounts = {}
+
+  // add base categories to word counts map
+  for (let category of Object.keys(Words)) {
+    wordCounts[category] = Words[category].length
+  }
+  // console.log(wordCounts)
+
+  // add word groups to word counts map
+  // console.log('counting group contents...')
+  // put all groups onto queue
+
+  // pop from queue, process
+  // if error, push to end of queue
+  let groupQueue = []
+  for (let group of Object.keys(WordGroups)) {
+    groupQueue.push(group)
+  }
+  // console.log(groupQueue)
+
+  let niaveCycleCounter = 0
+
+  while (groupQueue.length) {
+    if (niaveCycleCounter > 50) {
+      console.log(`cycle likely present. aborting.`)
+      break
+    }
+
+    let group = groupQueue.shift()
+    // console.log(`pulled ${group} from group queue...`)
+    let totalWords = 0
+    for (let category of WordGroups[group]) {
+      if (Object.hasOwn(wordCounts, category)) {
+        totalWords += wordCounts[category]
+      } else {
+        console.log(`----------couldn't find count for ${category} used in ${group} group, putting ${group} in back of queue ---------`)
+        groupQueue.push(group)
+        niaveCycleCounter++
+        totalWords = 0
+        // console.log(`cycle count: ${niaveCycleCounter}`)
+        break
+      }
+    }
+
+    if (totalWords > 0) {
+      wordCounts[group] = totalWords
+      niaveCycleCounter = 0
+    }
+  }
+  // console.log('counted group contents.')
+  // console.log(wordCounts)
+  return wordCounts
+}
+
+function compileWordGroupsNew(wordCounts) {
+  const compiledWordGroups = {}
+
+  for (let group of Object.keys(WordGroups)) {
+    totalWords = 0
+    const compiledGroup = {}
+
+    for (let category of WordGroups[group]) {
+      if (!wordCounts[category]) continue
+
+      totalWords += wordCounts[category]
+      compiledGroup[(totalWords - 1)] = category
+    }
+
+    compiledWordGroups[group] = {
+      categoryMap: compiledGroup,
+      totalWords: totalWords
+    }
+  }
+
+  console.log(compiledWordGroups)
+
+  return compiledWordGroups
+}
 
 function compileWordGroups() {
   const compiledWordGroups = {}
