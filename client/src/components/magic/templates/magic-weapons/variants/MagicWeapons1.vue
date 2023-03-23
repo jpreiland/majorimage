150dfb8bad9abf7773d2a21a2155e860ef524bbc
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2023-03-23 00:15:07 -0700

    added description section to magic weapon template, added more words and formats to data set

--- client/src/components/magic/templates/magic-weapons/variants/MagicWeapons1.vue
+++ client/src/components/magic/templates/magic-weapons/variants/MagicWeapons1.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="card-header">
     <div class="card-header-left">
+      <h2 class="card-title">Magic Weapon Description</h2>
+    </div>
+  </div>
+  <div class="info-panel-description">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+  </div>
+  <div class="card-header">
+    <div class="card-header-left">
       <h2 class="card-title">Magic Weapons</h2>
     </div>
   </div>
@@ -24,7 +37,7 @@ export default {
   inject: ['data'],
   data() {
     return {
-      numWeapons: 18,
+      numWeapons: 10,
       weapons: [],
       rerollToggle: true
     }
