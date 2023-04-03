531614c076c9f1fb46f6d1e76a7339504b4e32c2
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2023-04-02 22:50:46 -0700

    added a few descriptor types and more data

--- client/src/components/objects/templates/art/variants/Art1.vue
+++ client/src/components/objects/templates/art/variants/Art1.vue
@@ -6,6 +6,11 @@
   </div>
   <div class="info-panel-description">
       <p>
+        The work evokes themes of 
+        <Descriptor :type="'_VirtuePositive'" :color="'#65d1db'"/>
+        and 
+        <Descriptor :type="'_VirtueNegative'" :color="'#8f2f34'"/>
+        . <br />
         Lorem ipsum <Descriptor :type="'TestNum5-17'" :color="'#00ffff'"/> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <Descriptor :type="'TestNum0-9-no'" :color="'#ff00ff'"/> incididunt ut labore et dolore magna aliqua. 
         Ut enim ad  minim veniam, quis nostrud exercitation ullamco laboris <Descriptor :type="'TestNum5-17'" :color="'#ff0000'" :numRangeOverride="{min: 0, max: 3, zeroString: 'nothing!'}"/> nisi ut aliquip ex ea commodo consequat. 
         Duis <Descriptor :type="'EnhancementBonus'" :color="'#00ff00'"/> Sword aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -32,25 +37,28 @@
 </template>
 
 <script>
+import Descriptor from '@/components/descriptors/Descriptor.vue';
+
 export default {
-  name: 'Blacksmith1',
-  inject: ['data'],
-  data() {
-    return {
-      numArtwork: 10,
-      artwork: [],
-      rerollToggle: true
-    }
-  },
-  mounted() {
-    for (let i = 0; i < this.numArtwork; i++) {
-      this.artwork.push({id: i})
-    }
-  },
-  methods: {
-    async rerollAll() {
-      this.rerollToggle = !this.rerollToggle
-    }
-  }
+    name: "Blacksmith1",
+    inject: ["data"],
+    data() {
+        return {
+            numArtwork: 10,
+            artwork: [],
+            rerollToggle: true
+        };
+    },
+    mounted() {
+        for (let i = 0; i < this.numArtwork; i++) {
+            this.artwork.push({ id: i });
+        }
+    },
+    methods: {
+        async rerollAll() {
+            this.rerollToggle = !this.rerollToggle;
+        }
+    },
+    components: { Descriptor }
 }
 </script>
