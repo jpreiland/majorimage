847c6f4b10531389377631a0e654fea3c0beb004
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2026-06-18 13:53:02 -0700

    build-data script improvements

--- server/scripts/build-data.ts
+++ server/scripts/build-data.ts
@@ -8,6 +8,8 @@ import Woods from '../data/materials/woods.json'
 
 import type { CategoryName, GroupName } from '../../shared/types.ts'
 
+type CategoriesData = Record<CategoryName, string[]>
+
 const DATA_BASE_PATH = 'data'
 const MODULES_BASE_PATH = DATA_BASE_PATH + '/modules'
 const GENERATED_BASE_PATH = DATA_BASE_PATH + '/generated'
@@ -19,18 +21,38 @@ const GENERATED_METALS_PATH = GENERATED_BASE_PATH + '/metals.generated.json'
 const GENERATED_TEXTILES_PATH = GENERATED_BASE_PATH + '/textiles.generated.json'
 const GENERATED_WOODS_PATH = GENERATED_BASE_PATH + '/woods.generated.json'
 
+const categories: CategoriesData = structuredClone(Categories)
+const groups = structuredClone(Groups)
+const metals = structuredClone(Metals)
+const stones = structuredClone(Stones)
+const textiles = structuredClone(Textiles)
+const woods = structuredClone(Woods)
+
 function buildData() {
   try {
     const modules = fs.readdirSync(MODULES_BASE_PATH, { withFileTypes: true })
       .filter(entry => entry.isDirectory())
       .map(entry => entry.name)
+      .sort()
 
     for (const moduleName of modules) {
       loadModule(moduleName)
     }
+
+    writeGeneratedData()
+
   } catch (error) {
     console.error("Error loading data modules", error)
   }
+}
+
+function writeGeneratedData() {
+  fs.writeFileSync(GENERATED_CATEGORIES_PATH, stringifyData(categories))
+  fs.writeFileSync(GENERATED_GROUPS_PATH, stringifyData(groups))
+  fs.writeFileSync(GENERATED_METALS_PATH, JSON.stringify(metals, null, 2))
+  fs.writeFileSync(GENERATED_STONES_PATH, JSON.stringify(stones, null, 2))
+  fs.writeFileSync(GENERATED_TEXTILES_PATH, JSON.stringify(textiles, null, 2))
+  fs.writeFileSync(GENERATED_WOODS_PATH, JSON.stringify(woods, null, 2))
 }
 
 function loadModule(moduleName: string) {
@@ -38,140 +60,96 @@ function loadModule(moduleName: string) {
   const modulePath = `${MODULES_BASE_PATH}/${moduleName}`
   const files = fs.readdirSync(modulePath)
 
-  if (files.includes('categories.json')) {
-    loadModuleCategories(`${modulePath}/categories.json`)
-    const categoriesString = stringifyData(Categories)
-    fs.writeFileSync(GENERATED_CATEGORIES_PATH, categoriesString)
-  }
+  if (files.includes('categories-replace.json')) loadModuleReplacementCategories(`${modulePath}/categories-replace.json`)
+  if (files.includes('categories.json')) loadModuleCategories(`${modulePath}/categories.json`)
+  if (files.includes('categories-exclude.json')) loadModuleExclusionsCategories(`${modulePath}/categories-exclude.json`)
 
-  if (files.includes('categories-exclude.json')) {
-    loadModuleExclusionsCategories(`${modulePath}/categories-exclude.json`)
-    const categoriesString = stringifyData(Categories)
-    fs.writeFileSync(GENERATED_CATEGORIES_PATH, categoriesString)
-  }
+  if (files.includes('groups.json')) loadModuleGroups(`${modulePath}/groups.json`)
 
-  if (files.includes('categories-replace.json')) {
-    loadModuleReplacementCategories(`${modulePath}/categories-replace.json`)
-    const categoriesString = stringifyData(Categories)
-    fs.writeFileSync(GENERATED_CATEGORIES_PATH, categoriesString)
-  }
-
-  if (files.includes('groups.json')) {
-    loadModuleGroups(`${modulePath}/groups.json`)
-    const groupsString = stringifyData(Groups)
-    fs.writeFileSync(GENERATED_GROUPS_PATH, groupsString)
-  }
-
-  if (files.includes('metals.json')) {
-    loadModuleMaterials(`${modulePath}/metals.json`, Metals)
-    const metalsString = JSON.stringify(Metals, null, 2)
-    fs.writeFileSync(GENERATED_METALS_PATH, metalsString)
-  }
-
-  if (files.includes('stones.json')) {
-    loadModuleMaterials(`${modulePath}/stones.json`, Stones)
-    const stonesString = JSON.stringify(Stones, null, 2)
-    fs.writeFileSync(GENERATED_STONES_PATH, stonesString)
-  }
-
-  if (files.includes('textiles.json')) {
-    loadModuleMaterials(`${modulePath}/textiles.json`, Textiles)
-    const textilesString = JSON.stringify(Textiles, null, 2)
-    fs.writeFileSync(GENERATED_TEXTILES_PATH, textilesString)
-  }
-
-  if (files.includes('woods.json')) {
-    loadModuleMaterials(`${modulePath}/woods.json`, Woods)
-    const woodsString = JSON.stringify(Woods, null, 2)
-    fs.writeFileSync(GENERATED_WOODS_PATH, woodsString)
-  }
+  if (files.includes('metals.json')) loadModuleMaterials(`${modulePath}/metals.json`, metals)
+  if (files.includes('stones.json')) loadModuleMaterials(`${modulePath}/stones.json`, stones)
+  if (files.includes('textiles.json')) loadModuleMaterials(`${modulePath}/textiles.json`, textiles)
+  if (files.includes('woods.json')) loadModuleMaterials(`${modulePath}/woods.json`, woods)
 }
 
 function loadModuleCategories(path: string) {
-  if (fs.statSync(path).isFile()) {
-    const rawData = fs.readFileSync(path, 'utf8')
-    const moduleCategories = JSON.parse(rawData)
+  const rawData = fs.readFileSync(path, 'utf8')
+  const moduleCategories = JSON.parse(rawData)
 
-    for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
-      if (!Object.keys(Categories).includes(categoryName)) {
-        Categories[categoryName] = moduleCategories[categoryName]
-      } else {
-        augmentCategory(categoryName, moduleCategories[categoryName])
-      }
+  for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
+    if (!(categoryName in categories)) {
+      categories[categoryName] = moduleCategories[categoryName]
+    } else {
+      augmentCategory(categoryName, moduleCategories[categoryName])
     }
   }
 }
 
 function augmentCategory(categoryName: CategoryName, words: string[]) {
+  const categoryWordSet = new Set(categories[categoryName])
+
   for (const word of words) {
-    // @ts-expect-error
-    if (!Categories[categoryName].includes(word)) Categories[categoryName].push(word)
+    if (!categoryWordSet.has(word)) categories[categoryName].push(word)
   }
 }
 
 function loadModuleExclusionsCategories(path: string) {
-  if (fs.statSync(path).isFile()) {
-    const rawData = fs.readFileSync(path, 'utf8')
-    const moduleCategories = JSON.parse(rawData)
+  const rawData = fs.readFileSync(path, 'utf8')
+  const moduleCategories = JSON.parse(rawData)
 
-    for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
-      if (Object.keys(Categories).includes(categoryName)) {
-        removeWordsFromCategory(categoryName, moduleCategories[categoryName])
-      }
+  for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
+    if (categoryName in categories) {
+      removeWordsFromCategory(categoryName, moduleCategories[categoryName])
     }
-  }
+  }  
 }
 
 function removeWordsFromCategory(categoryName: CategoryName, words: string[]) {
   const removeWordsSet = new Set(words)
 
-  for (let i = Categories[categoryName].length - 1; i >= 0; i--) {
-    if (removeWordsSet.has(Categories[categoryName][i])) Categories[categoryName].splice(i, 1)
+  for (let i = categories[categoryName].length - 1; i >= 0; i--) {
+    if (removeWordsSet.has(categories[categoryName][i])) categories[categoryName].splice(i, 1)
   }
 }
 
 function loadModuleReplacementCategories(path: string) {
-  if (fs.statSync(path).isFile()) {
-    const rawData = fs.readFileSync(path, 'utf8')
-    const moduleCategories = JSON.parse(rawData)
+  const rawData = fs.readFileSync(path, 'utf8')
+  const moduleCategories = JSON.parse(rawData)
 
-    for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
-      if (Object.keys(Categories).includes(categoryName)) {
-        Categories[categoryName] = moduleCategories[categoryName]
-      }
+  for (const categoryName of Object.keys(moduleCategories) as CategoryName[]) {
+    if (categoryName in categories) {
+      categories[categoryName] = moduleCategories[categoryName]
     }
   }
 }
 
 function loadModuleGroups(path: string) {
-  if (fs.statSync(path).isFile()) {
-    const rawData = fs.readFileSync(path, 'utf8')
-    const moduleGroups = JSON.parse(rawData)
+  const rawData = fs.readFileSync(path, 'utf8')
+  const moduleGroups = JSON.parse(rawData)
 
-    for (const groupName of Object.keys(moduleGroups) as GroupName[]) {
-      if (!Object.keys(Groups).includes(groupName)) {
-        Groups[groupName] = moduleGroups[groupName]
-      } else {
-        augmentGroup(groupName, moduleGroups[groupName])
-      }
+  for (const groupName of Object.keys(moduleGroups) as GroupName[]) {
+    if (!(groupName in groups)) {
+      groups[groupName] = moduleGroups[groupName]
+    } else {
+      augmentGroup(groupName, moduleGroups[groupName])
     }
   }
 }
 
 function augmentGroup(groupName: GroupName, groupMembers: string[]) {
+  const groupMemberSet = new Set(groups[groupName])
+
   for (const groupMember of groupMembers) {
-    if (!Groups[groupName].includes(groupMember)) Groups[groupName].push(groupMember)
+    if (!groupMemberSet.has(groupMember)) groups[groupName].push(groupMember)
   }
 }
 
 function loadModuleMaterials(path: string, baseMaterials: object) {
-  if (fs.statSync(path).isFile()) {
-    const rawData = fs.readFileSync(path, 'utf8')
-    const moduleMaterials = JSON.parse(rawData)
+  const rawData = fs.readFileSync(path, 'utf8')
+  const moduleMaterials = JSON.parse(rawData)
 
-    if (Array.isArray(moduleMaterials) && Array.isArray(baseMaterials)) {
-      baseMaterials.push(...moduleMaterials)
-    }
+  if (Array.isArray(moduleMaterials) && Array.isArray(baseMaterials)) {
+    baseMaterials.push(...moduleMaterials)
   }
 }
 
