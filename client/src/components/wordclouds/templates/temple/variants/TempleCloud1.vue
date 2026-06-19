350112a51190bc285ba01ecf2001642b6a0c229b
JP Reiland <3701341+jpreiland@users.noreply.github.com>
2026-06-19 00:39:45 -0700

    spacing fix

--- client/src/components/wordclouds/templates/temple/variants/TempleCloud1.vue
+++ client/src/components/wordclouds/templates/temple/variants/TempleCloud1.vue
@@ -11,11 +11,11 @@ import type { WordCloudItem } from '../../../word-cloud/word-cloud'
 const descriptorConfigs = ref<WordCloudItem[]>([
   { id: "temple-name", component: 'descriptor', type: 'Temple', color: 'green' },
   { id: "color", component: 'simple', type: 'colorAll', color: ['red', 'orange', 'yellow', 'green', 'blue', 'violet'], properNoun: true },
-  { id: "adjective", component: 'simple', type: 'adjectivePerson', color: 'black', properNoun: true  },
-  { id: "trade", component: 'simple', type: 'tradeAll', color: 'blue', properNoun: true  },
-  { id: "animal", component: 'simple', type: 'animal', color: 'orange', properNoun: true  },
-  { id: "structure", component: 'simple', type: 'structure', color: 'red', properNoun: true  },
-  { id: "item", component: 'simple', type: 'itemAll', color: 'purple', properNoun: true  }
+  { id: "adjective", component: 'simple', type: 'adjectivePerson', color: 'black', properNoun: true },
+  { id: "trade", component: 'simple', type: 'tradeAll', color: 'blue', properNoun: true },
+  { id: "animal", component: 'simple', type: 'animal', color: 'orange', properNoun: true },
+  { id: "structure", component: 'simple', type: 'structure', color: 'red', properNoun: true },
+  { id: "item", component: 'simple', type: 'itemAll', color: 'purple', properNoun: true }
 ])
 
 </script>
